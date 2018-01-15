c6409d34fb255d417f1c2278c57ccfffd49ca7df
Manoj Tutika <manojvenkat92@gmail.com>
2018-01-15 17:47:35 +0530

    All RESTful methods implemented. Going ahead with APIs.

--- python-server/server.py
+++ python-server/server.py
@@ -35,6 +35,33 @@ class ServerModule(BaseHTTPRequestHandler):
         return
 
 
+    def do_PUT(self):
+        self.send_response(200)
+
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+
+        # Send message back to client
+        message = "Updating world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
+
+    def do_DELETE(self):
+        self.send_response(200)
+
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+
+        # Send message back to client
+        message = "Deleting world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
 def run():
     print('starting server...')
 
